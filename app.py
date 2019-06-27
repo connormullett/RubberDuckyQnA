ca42c04fcb2abf6b41ad77e7bf94fa1eb270859e
connor <connormullett@gmail.com>
2019-06-27 18:33:18 -0400

    app to application

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import os
 from rubber_ducky.src import create_app, db
 from rubber_ducky import user_api
 
-app = create_app(os.getenv('FLASK_ENV'))
-app.register_blueprint(user_api)
+application = create_app(os.getenv('FLASK_ENV'))
+application.register_blueprint(user_api)
 
-app.app_context().push()
+application.app_context().push()
 
-app.run()
+application.run()
