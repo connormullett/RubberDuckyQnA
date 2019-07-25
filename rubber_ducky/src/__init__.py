5e5cf9b225bc873a9353fb332798b6cfb801ca06
Connor Mullett <connormullett@gmail.com>
2019-07-24 21:49:48 -0400

    added cors compatibility

--- rubber_ducky/src/__init__.py
+++ rubber_ducky/src/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 from .config import app_config
 
@@ -12,6 +13,7 @@ bcrypt = Bcrypt()
 def create_app(env_name):
     app = Flask(__name__)
     app.config.from_object(app_config[env_name])
+    CORS(app)
     db.init_app(app)
     bcrypt.init_app(app)
 
